b3b849f56d0afcfb3647ca64050ce15709d72fdc
CS193P student <yiu.jackie@gmail.com>
2020-08-18 17:40:14 -0400

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by ENRAG3DCHICKEN on 2020-08-18.
+//  Copyright © 2020 ENRAG3DCHICKEN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
